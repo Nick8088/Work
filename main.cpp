277b93a66adf09f2afa9920b2b0b5c2e9355accd
Nick8088 <yannick.roach@yahoo.com>
2021-03-02 14:22:14 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,8 @@
+//Function overloading = Same name functions doing different tasks
+//You can use different operators instead of the "+"
+//Defdault constructors are things
+//
+//Opertors are still functions
+//There's mathematical vectors
+//Operators are simply a functions
+//
